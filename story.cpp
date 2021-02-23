d96eaacfbfb136ef30a8ae0d0bfb2a8c6e569f00
Marc Petitdemange <marc.petitdemange57@gmail.com>
2021-02-23 16:09:49 +0100

    Ajout d'un fichier image

--- story.cpp
+++ story.cpp
@@ -1,14 +1,26 @@
 #include "story.h"
 #include "ui_story.h"
+#include <QPropertyAnimation>
 
 Story::Story(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Story)
 {
     ui->setupUi(this);
+
 }
 
 Story::~Story()
 {
     delete ui;
+}
+
+void Story::on_pushButton_clicked()
+{
+    this->centralWidget()->setStyleSheet("QWidget#storyBackground {background-image: url(':images/Images/CitySahri.jpg')}");
+}
+
+void Story::on_pushButton_2_clicked()
+{
+
 }
--- story.h
+++ story.h
@@ -15,6 +15,11 @@ public:
     explicit Story(QWidget *parent = 0);
     ~Story();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
 private:
     Ui::Story *ui;
 };
